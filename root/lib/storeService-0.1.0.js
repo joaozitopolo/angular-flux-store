10213c74b52cf0aa0fc99b486125d7cea0128a09
joaozitopolo <joaozito.polo@gmail.com>
2016-08-26 23:06:10 -0300

    redistribuition for fonts

---
+++ root/lib/storeService-0.1.0.js
@@ -0,0 +1,60 @@
+/** store service for Angular 1.x with Javascript EC5 */
+
+(function(angular) {
+	
+	var module = angular.module('fStoreService', []);
+	
+	module.service('fStore', fStore);
+    module.controller('fStoreController', ['fStore', fStoreBasicController]);
+	
+	function fStore() {
+
+		this.get = get;
+		this.update = update;
+		this.subscribe = subscribe;
+		this.unsubscribe = unsubscribe;
+
+		var data = {};
+		var listeners = [];
+
+		function get() {
+			return angular.extend({}, data);
+		}
+		
+		function update(incomingData) {
+			if(angular.isObject(incomingData)) {
+				var oldData = data;
+				data = angular.extend({}, data, incomingData);
+				callListeners(data, oldData);
+			}
+			return data;
+		}
+
+		function subscribe(listener/*(data, oldData)*/) {
+			listeners.push(listener);
+			listener(get(), {});
+			return function() { unsubscribe(listener); };
+		}
+
+		function unsubscribe(listener) {
+			var idx = listeners.indexOf(listener);
+			if(idx >= 0) {
+				listeners.splice(idx, 1);
+			}
+		}
+
+		function callListeners(data, oldData) {
+			angular.forEach(listeners, function (listener) {
+				listener(get(), oldData);
+			});
+		}
+
+	}
+
+    function fStoreBasicController(store) {
+        var self = this;
+        self.$destroy = store.subscribe(function(data) { self.data = data; });
+        self.update = function() { store.update(self.data); };
+    }
+	
+})(angular);
